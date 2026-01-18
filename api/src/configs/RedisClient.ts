441aaea09a7c0635252c58cdb6509f72b3dfc4f0
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2026-01-18 18:22:38 +0530

    creating singleton redisclient wrapper

---
+++ api/src/configs/RedisClient.ts
@@ -0,0 +1,48 @@
+import Redis from "ioredis"
+const REDIS_URL="redis://localhost:6379"
+import { v4 as uuidv4 } from "uuid";
+class RedisClient {
+    private client:Redis;
+    private publisher:Redis;
+    private static instance:RedisClient;
+
+    private constructor(){
+        this.client=new Redis(REDIS_URL);
+        this.client.on("connect",()=>{
+            console.log("Redis is connected");
+        })
+        this.client.on("error",()=>{
+            console.log("Error while connecting redis client");
+        })
+        this.publisher=new Redis(REDIS_URL);
+         this.publisher.on("connect",()=>{
+            console.log("publisher is connected");
+        })
+        this.publisher.on("error",()=>{
+            console.log("Error while connecting publisher client");
+        })
+    }
+    public static getInstance(){
+        if(!this.instance){
+            this.instance=new RedisClient();
+        }
+        return this.instance;
+    }
+
+    public sendAndawait(message:any){
+        return new Promise((resolve)=>{
+            const id=this.generateRandomID();
+            this.client.subscribe(id,(message:any)=>{
+                this.client.unsubscribe(id);
+                resolve(JSON.parse(message));
+            })
+            this.publisher.rpush("message",JSON.stringify({clientId:id,message}));
+        })
+    }
+    public generateRandomID(){
+        const id=uuidv4();
+        return id;
+    }
+}
+
+export default RedisClient;
---
+++ api/src/types/types.ts
@@ -0,0 +1,3 @@
+export const CREATE_ORDER="create_order";
+export const CANCEL_ORDER="cancel_order";
+export const GET_OPEN_ORDERS="get_orders";
