babd428712bf496f994c297044c76a655c59f090
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2026-01-19 09:41:11 +0530

    added entry file code

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,11 @@
 import express from "express"
+import { ordersRouter } from "./routes/orders.route"
+const app=express();
+app.use(express.json());
+const PORT=process.env.PORT || 3000;
+
+app.use("/api/v1/orders",ordersRouter);
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+}
+);
